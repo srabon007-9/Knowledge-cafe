8b50b42b5c31028798492cd94743688fffa9d2c8
srabon4u <srabonahmed2002@gmail.com>
2024-11-01 19:59:44 +0600

    Second

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect( ()=>{
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+  return (
+    <div className="md:w-2/3">
+      <h2 className="text-4-xl">Blogs: {blogs.length}</h2>
+    </div>
+  )
+}
+
+export default Blogs
